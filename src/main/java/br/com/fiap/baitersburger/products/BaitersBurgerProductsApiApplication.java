fde00139a2596d1e2ddbf29922ab9bd1bdfcfd6a
Lucas Nabeto <lucas.nabeto@outlook.com>
2025-11-04 23:01:47 -0300

    env: initial setup

---
+++ src/main/java/br/com/fiap/baitersburger/products/BaitersBurgerProductsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baitersburger.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaitersBurgerProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaitersBurgerProductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/baitersburger/products/BaitersBurgerProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baitersburger.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitersBurgerProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
