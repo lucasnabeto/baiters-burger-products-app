637634ac3ae1bf18ba78b096ba808c80a3c0e756
Lucas Nabeto <lucas.nabeto@outlook.com>
2025-12-28 12:17:10 -0300

    test: configure H2 database and add tests for use cases and adapters

---
+++ src/test/java/br/com/fiap/baitersburger/products/core/application/usecase/InsertProductUseCaseTest.java
@@ -0,0 +1,15 @@
+package br.com.fiap.baitersburger.products.core.application.usecase;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class InsertProductUseCaseTest {
+
+    @Test
+    void insert() {
+    }
+}
