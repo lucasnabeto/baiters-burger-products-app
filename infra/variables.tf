77ce5084e3228b4677846066fe3d5777283789cf
Lucas Nabeto <lucas.nabeto@outlook.com>
2025-12-29 01:00:36 -0300

    feat: add a load balancer to ecs service

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,19 @@
 variable "docker_image_tag" {
   description = "Docker image tag (commit SHA)"
   type        = string
-  default = "latest"
+}
+
+variable "database_connection_url" {
+  description = "Database connection URL"
+  type        = string
+}
+
+variable "database_driver_class_name" {
+    description = "Database driver class name"
+    type        = string
+}
+
+variable "database_hibernate_dialect" {
+    description = "Database Hibernate dialect"
+    type        = string
 }
